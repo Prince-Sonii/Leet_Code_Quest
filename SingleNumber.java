de52fffe06cb67dbecbfcd83b52a00345778e3c4
prince <princesoni200325@gmail.com>
2024-05-22 23:41:47 +0530

    LeetCode Qno.136

---
+++ SingleNumber.java
@@ -0,0 +1,11 @@
+class SingleNumber {
+    //Qno.136 Single Number
+    //Solution using bit-wise operator
+    static int singleNumber(int[] nums) {
+        int ans =0;
+        for(int i=0;i<nums.length;i++) {
+            ans=ans^nums[i];
+        }
+        return ans;
+    }
+}
