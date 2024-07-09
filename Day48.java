0bf3ddc7d8c0633e72da0f062f3d2e4ef9c4d5ae
prince <princesoni200325@gmail.com>
2024-07-09 20:11:33 +0530

    LeetCode Qno.1464

---
+++ Day48.java
@@ -0,0 +1,17 @@
+class Day48 {
+    //Qno. 1464 Maximum Product of Two Elements in an Array
+    //Solution
+    public int maxProduct(int[] nums) {
+        int max = 0;
+        int secondMax =0;
+        for(int i=0;i<nums.length;i++) {
+            if(nums[i]>max) {
+                secondMax = max;
+                max = nums[i];
+            } else if(nums[i]>secondMax) {
+                secondMax = nums[i];
+            }
+        }
+        return (max-1)*(secondMax-1); 
+    }
+}
