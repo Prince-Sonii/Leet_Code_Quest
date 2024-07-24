ca43137f92ec1f4486b294ac46f4483290136edb
prince <princesoni200325@gmail.com>
2024-07-24 22:36:43 +0530

    LeetCode Qno.14

---
+++ Day63.java
@@ -0,0 +1,19 @@
+class Day63 {
+    //Qno. 14 Longest Common Prefix
+    //Solution
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String prefix = strs[0]; 
+        for (int i = 1; i < strs.length; i++) {
+            while ( strs[i].indexOf(prefix) != 0 ) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+            }
+            if ( prefix.length() == 0 ) {
+                return "";
+            }
+        }
+        return prefix;
+    }
+}
