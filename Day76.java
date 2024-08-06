3cc754873322b74351c02ed19596b6390ffe7c68
prince <princesoni200325@gmail.com>
2024-08-06 22:32:11 +0530

    LeetCode Qno.1759

---
+++ Day76.java
@@ -0,0 +1,31 @@
+class Day76 {
+    //Qno. 1759 Count Number of Homogenous Substrings
+    //Solution
+    public int countHomogenous(String s) {
+        long res  = 0;
+        char[] c = s.toCharArray();
+        int start =0;
+        for(int i=0;i<c.length;i++)
+        {
+            if(c[i]!=c[start])
+            {
+                int appear = i-start;
+                while(appear>0)
+                {
+                    res+=appear;
+                    appear-=1;
+                }
+                start=i;
+            }
+        }
+
+        int appear = c.length-start;
+        while(appear>0)
+        {
+            res+=appear;
+            appear-=1;
+        }
+
+        return (int)(res%(Math.pow(10,9)+7));
+    }
+}
