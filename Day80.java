b3e577d59e4d3eecd050fe1e13679ef30315ab15
prince <princesoni200325@gmail.com>
2024-08-10 22:40:20 +0530

    LeetCode Qno.1545

---
+++ Day80.java
@@ -0,0 +1,22 @@
+class Day80 {
+    //Qno. 1545. Find Kth Bit in Nth Binary String
+    //Solution
+    public char findKthBit(int n, int k) {
+        if(n == 1){
+            return '0';
+        }
+        
+        int numberOfColumns = (int)Math.pow(2,n) - 1;
+        int mid = numberOfColumns/2;
+                
+        if(k <= mid){
+            return findKthBit(n-1,k);
+        } else if (k == mid + 1){ 
+            return '1';
+        } else {
+           k = numberOfColumns - k + 1;
+           char c = findKthBit(n-1,k);
+           return c == '0' ? '1' : '0';
+        }
+}
+}
