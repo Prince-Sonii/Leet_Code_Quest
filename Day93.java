c769e7d5feec74022e97f9dd70c0483782b51d01
prince <princesoni200325@gmail.com>
2024-08-23 23:45:42 +0530

    LeetCode Qno.476

---
+++ Day93.java
@@ -0,0 +1,13 @@
+class Day93 {
+    //Qno. 476 Number Complement
+    //Solution
+    public int findComplement(int num) {
+        if (num == 0) return 1;
+
+        int bitLength = Integer.toBinaryString(num).length();
+        
+        int mask = (1 << bitLength) - 1;
+        
+        return num ^ mask;
+    }
+}
